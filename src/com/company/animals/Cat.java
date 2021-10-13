fa2f3ffd3991842d6256ce08b2a4307c778411ea
Kris <kristof.solak@gmail.com>
2021-10-13 18:06:23 +0200

    Constructor added

--- src/com/company/animals/Cat.java
+++ src/com/company/animals/Cat.java
@@ -4,8 +4,18 @@ public class Cat extends Mammal{
 
 
     //Attribute
+    private String breed;
+    private int weight;
+    private String sex;
 
     //Constructor
+
+    public Cat(String name, String breed, int weight, String sex){
+        setName(name);
+        this.breed = breed;
+        this.weight = weight;
+        this.sex = sex;
+    }
 
     //Methods
 
--- src/com/company/animals/Main.java
+++ src/com/company/animals/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cat cat = new Cat();
-        cat.setName("Tidles");
+        Cat cat = new Cat("Snuggles", "Black", 2, "female");
+
         System.out.println(cat.getName());
 
         cat.breath();
